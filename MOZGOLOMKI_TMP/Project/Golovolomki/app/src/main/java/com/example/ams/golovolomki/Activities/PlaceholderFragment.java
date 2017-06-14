b913332c7b058ecfbd418912fd86f594bd13cc5c
Artem Shishkin <AMShishkin.vrn@gmail.com>
2017-06-14 14:51:17 +0300

    Fixed crash on open Puzzle activity

--- MOZGOLOMKI_TMP/Project/Golovolomki/app/src/main/java/com/example/ams/golovolomki/Activities/PlaceholderFragment.java
+++ MOZGOLOMKI_TMP/Project/Golovolomki/app/src/main/java/com/example/ams/golovolomki/Activities/PlaceholderFragment.java
@@ -3,7 +3,6 @@ package com.example.ams.golovolomki.Activities;
 import android.content.Context;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
@@ -96,38 +95,21 @@ public class PlaceholderFragment extends Fragment {
         else textView.setText("☆");
         textView.setTypeface(Typefaces.get(inflater.getContext(), "fonts/cavia_puzzle.ttf"));
 
-        if (BuildConfig.DEBUG) {
-            AdView adContainer = (AdView) rootView.findViewById(R.id.adView);
-            AdView mAdView = new AdView(rootView.getContext());
-            mAdView.setAdSize(adSize);
-            mAdView.setAdUnitId("ca-app-pub-9188008799728984/3543487752");
-            adContainer.addView(mAdView);
-            AdRequest adRequest = new AdRequest.Builder()
-                    .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                    .addTestDevice("05B14F908C488B9820E02532746B28A1") // [Genymotion] Nexus 7
-                    .addTestDevice("DCB08892F1089BCA2AB568F6009342A7") // [Genymotion] Galaxy S5
-                    .addTestDevice("EE9FA0F13B20B55ADD88A77A9FBECC3F") // [Genymotion] CP 768x1280
-                    .addTestDevice("3F7F5FE726961D6E2600EF0E7D278EC2") // [Genymotion] CP 480x800
-                    .addTestDevice("AA0714F5CF37C6C8EA09E8E5E8BFF156") // [Genymotion] Nexus 7 800x1280
-                    .addTestDevice("91AC0C17F5D5838404785E3EBA501184") // Galaxy Tab E
-                    .addTestDevice("616AF26EDDBBEBAF18659E076AD01080") // Galaxy s3
-                    .addTestDevice("2677F1E886AFD81B7E78E0B243422EF0") // Nexus 6
-                    .addTestDevice("BEA3544ED9561600F831EE6B5A414F49") // Galaxy A3 2016
-                    .addTestDevice("27E76A584DAB315A77C658037558DDDA") // Fly iq4410
-                    .addTestDevice("7CADE54B631E24530684B71F6B699170") // Nexus 7
-                    .addTestDevice("B2DD43156D4EC31C1224559A0D77CFAB") // Mi4
-                    .addTestDevice("5D3F192EF9029C015917D33F89F2A99A") // Shield
-                    .addTestDevice("328CA476D709D83BFFCF8E03B9AF0584") // LG Optimus L70
-                    .build();
-            mAdView.loadAd(adRequest);
-        }
-        else
-        {
-            AdView mPublisherAdView = (AdView) rootView.findViewById(R.id.adView);
-            mPublisherAdView.setAdUnitId("ca-app-pub-9188008799728984/3543487752");
-            AdRequest adRequest = new  AdRequest.Builder().build();
-            mPublisherAdView.loadAd(adRequest);
-        }
+        // set admob block
+        AdView _adContainer = (AdView) rootView.findViewById(R.id.adView);
+        AdView _mAdView = new AdView(rootView.getContext());
+        _mAdView.setAdSize(adSize);
+        _mAdView.setAdUnitId("ca-app-pub-9188008799728984/3543487752");
+        _adContainer.addView(_mAdView);
+        AdRequest _adRequest;
+
+        if (BuildConfig.DEBUG) _adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice("BEA3544ED9561600F831EE6B5A414F49") // Galaxy A3 2016
+                .build();
+        else _adRequest = new AdRequest.Builder().build();
+
+        _mAdView.loadAd(_adRequest);
 
         return rootView;
     }
--- MOZGOLOMKI_TMP/Project/Golovolomki/app/src/main/java/com/example/ams/golovolomki/Activities/PuzzleActivity.java
+++ MOZGOLOMKI_TMP/Project/Golovolomki/app/src/main/java/com/example/ams/golovolomki/Activities/PuzzleActivity.java
@@ -8,7 +8,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import com.example.ams.golovolomki.Alert.Alert;
 import com.example.ams.golovolomki.Alert.Alerter;
 import com.example.ams.golovolomki.Assistants.DatabaseHelper;
